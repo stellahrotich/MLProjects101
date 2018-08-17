ed601302b9c31f92c5041a02bd95021cb6417e72
Stellah Rotich <37912118+stellahrotich@users.noreply.github.com>
2018-08-17 18:37:31 +0300

    Create grading.py

---
+++ grading.py
@@ -0,0 +1,13 @@
+#program to give grades when a student enters  mark scored
+
+marks = float(input("Please enter your marks I give you your grade:"))
+if marks >= 70:
+    print ("A")
+elif marks >= 60:
+    print ("B")
+elif marks >= 50:
+    print ("C")
+elif marks >= 40:
+    print ("D")
+else:
+    print ("FAIL")
