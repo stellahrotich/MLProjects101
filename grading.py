d1a973af0d6767c833399c2da74f44230f7cd829
Stellah Rotich <37912118+stellahrotich@users.noreply.github.com>
2018-08-17 18:53:36 +0300

    Update grading.py

--- grading.py
+++ grading.py
@@ -1,13 +1,16 @@
 #program to give grades when a student enters  mark scored
 
 marks = float(input("Please enter your marks I give you your grade:"))
-if marks >= 70:
-    print ("A")
-elif marks >= 60:
-    print ("B")
-elif marks >= 50:
-    print ("C")
-elif marks >= 40:
-    print ("D")
+if marks < 0 or marks > 100:
+    print ("Out of Range")
 else:
-    print ("FAIL")
+    if marks >= 70:
+        print ("A")
+    elif marks >= 60:
+        print ("B")
+    elif marks >= 50:
+        print ("C")
+    elif marks >= 40:
+        print ("D")
+    else:
+        print ("FAIL")
