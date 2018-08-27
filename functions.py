c4af68d9fa3bd225eeb433f9db52452901f6baf5
Stellah Rotich <37912118+stellahrotich@users.noreply.github.com>
2018-08-27 21:32:08 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -42,3 +42,17 @@ if numberofguesses <= 3:
             print "you guessed it!"
 else:    
     print("You have blocked your game")
+   
+#program to sort numbers
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+print(quicksort([3,6,8,10,1,2,1]))
+# Prints "[1, 1, 2, 3, 6, 8, 10]"
+
